44e18bcee01a548c50b54b34ea91890d585a77d0
iDubbbzTV3 <37233419+iDubbbzTV3@users.noreply.github.com>
2018-05-06 02:18:00 -0600

    Hice que fuera posible agregar contactos quemados

--- app/src/main/java/com/example/monte/directorio/FragmentContact.java
+++ app/src/main/java/com/example/monte/directorio/FragmentContact.java
@@ -1,16 +1,27 @@
 package com.example.monte.directorio;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class FragmentContact extends Fragment {
 
     View v;
+    private RecyclerView recyclerview;
+    private List<Contacts> contactlist;
+    private ContentResolver contentResolver;
 
     public FragmentContact() {
     }
@@ -19,6 +30,24 @@ public class FragmentContact extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.contact_fragment, container, false );
+        recyclerview = (RecyclerView) v.findViewById(R.id.contact_recycler);
+        RecyclerViewAdapter recyclerviewad = new RecyclerViewAdapter(getContext(), contactlist);
+        recyclerview.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerview.setAdapter(recyclerviewad);
         return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        contactlist = new ArrayList<>();
+        contactlist.add(new Contacts("Aaron Jones", "78716096", R.drawable.ic_call));
+
+    }
+
+
+    public ContentResolver getContentResolver() {
+        return contentResolver;
     }
 }
