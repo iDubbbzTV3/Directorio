c0f235284e46bb711993ff52e3ee1783b53329ed
iDubbbzTV3 <37233419+iDubbbzTV3@users.noreply.github.com>
2018-05-08 02:03:12 -0600

    Agregue una ventana que se abre al pulsar el contacto, el boton de llamada parece no funcionar, desconozco de la razon

--- app/src/main/java/com/example/monte/directorio/MainActivity.java
+++ app/src/main/java/com/example/monte/directorio/MainActivity.java
@@ -1,35 +1,65 @@
 package com.example.monte.directorio;
 
+import android.Manifest;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TabLayout tablayout;
     private ViewPager viewPager;
     private ViewPagerAdapter adapter;
+    public Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn1 = findViewById(R.id.diallup);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_CALL);
+                i.setData(Uri.parse("tel:76428144"));
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                startActivity(i);
+            }
+        });
+
 
         tablayout = (TabLayout) findViewById(R.id.tablayout);
         viewPager = (ViewPager) findViewById(R.id.viewpager);
         adapter = new ViewPagerAdapter(getSupportFragmentManager());
 
-        adapter.addFragment(new FragmentCall(), "Calls");
         adapter.addFragment(new FragmentContact(), "Contacts");
         adapter.addFragment(new FragmentFavoritos(), "Favorites");
 
         viewPager.setAdapter(adapter);
         tablayout.setupWithViewPager(viewPager);
 
-        tablayout.getTabAt(0).setIcon(R.drawable.ic_call);
-        tablayout.getTabAt(1).setIcon(R.drawable.ic_face);
-        tablayout.getTabAt(2).setIcon(R.drawable.ic_favorites);
+        tablayout.getTabAt(0).setIcon(R.drawable.ic_face);
+        tablayout.getTabAt(1).setIcon(R.drawable.ic_favorites);
     }
-
 }
--- app/src/main/java/com/example/monte/directorio/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/monte/directorio/RecyclerViewAdapter.java
@@ -1,13 +1,19 @@
 package com.example.monte.directorio;
 
+import android.app.Dialog;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -15,6 +21,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     Context context;
     List<Contacts> data;
+    Dialog dialog;
 
     public RecyclerViewAdapter(Context context, List<Contacts> data) {
         this.context = context;
@@ -27,7 +34,24 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
         View v;
         v = LayoutInflater.from(context).inflate(R.layout.contact_tags, parent, false);
-        ViewHolder vHolder = new ViewHolder(v);
+        final ViewHolder vHolder = new ViewHolder(v);
+
+        dialog = new Dialog(context);
+        dialog.setContentView(R.layout.caja_contacto);
+
+        vHolder.item_contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView dialog_tv = (TextView) dialog.findViewById(R.id.changename);
+                TextView dialog_phone = (TextView) dialog.findViewById(R.id.changenumber);
+                ImageView dialog_photo = (ImageView) dialog.findViewById(R.id.photo);
+                dialog_tv.setText(data.get(vHolder.getAdapterPosition()).getName());
+                dialog_phone.setText(data.get(vHolder.getAdapterPosition()).getPhone());
+                dialog_photo.setImageResource(data.get(vHolder.getAdapterPosition()).getPhoto());
+                dialog.show();
+            }
+
+        });
 
         return vHolder;
     }
@@ -41,6 +65,8 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     }
 
+
+
     @Override
     public int getItemCount() {
         return data.size();
@@ -51,6 +77,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         private TextView textviewName;
         private TextView textviewPhone;
         private ImageView img;
+        private LinearLayout item_contact;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -58,6 +85,7 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
             textviewName = (TextView) itemView.findViewById(R.id.contactID);
             textviewPhone = (TextView) itemView.findViewById(R.id.contactNumber);
             img = (ImageView) itemView.findViewById(R.id.person);
+            item_contact = (LinearLayout) itemView.findViewById(R.id.contact_item_id);
 
         }
     }
